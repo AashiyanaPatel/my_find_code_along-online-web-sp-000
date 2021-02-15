6078b00292e3963baefdc6719a4f5b31208efbb6
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-15 22:03:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,11 +4,10 @@ def my_find(collection)
   i = 0
   while i <
   collection.length
-    if
-  yield(collection[i])
     return
+  yield(collection[i])
   collection[i]
-end
+  end
   i = i + 1
   end
 end
